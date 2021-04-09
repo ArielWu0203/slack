c9838fdfa865b7a7aa99cc88ba4a9432879d2b76
ArielWu0203 <lucky020372@gmail.com>
2021-04-10 00:39:20 +0800

    Authentication

--- src/main.js
+++ src/main.js
@@ -10,13 +10,7 @@ Vue.config.productionTip = false
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 var firebaseConfig = {
-  apiKey: "AIzaSyDSm3T3Lq51IKrj6ZplvVHTqvwQnv5E4Cs",
-  authDomain: "vueslack-9a364.firebaseapp.com",
-  projectId: "vueslack-9a364",
-  storageBucket: "vueslack-9a364.appspot.com",
-  messagingSenderId: "198112836455",
-  appId: "1:198112836455:web:34078e0982c82a0cad0dc2",
-  measurementId: "G-XJJF6FS3SJ"
+
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
